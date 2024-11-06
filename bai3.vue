11eafabfe486a90cb75cec0cfef2d5f28c4b45ae
Hhanba <152430515+Hhanba@users.noreply.github.com>
2024-11-07 03:27:11 +0700

    Add files via upload

---
+++ bai3.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>{{ greeting }}</h1>
+    <button @click="changeGreeting">Click de thay doi</button>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const greeting = ref("Xin chao ban!");
+const changeGreeting = ()=>{
+    greeting.value = "hello world!";
+}
+</script>
